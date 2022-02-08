1f50fd790c82e9fb34bbd1577f646d41c1a9bbee
Edsol <edoardo.soloperto@gmail.com>
2022-02-08 22:44:10 +0100

    Add terminal settings inside readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # dotfiles
+
 :closed_book: personal dotfiles, configurations and many more
+
+---
+
+Terminal configs:
+
+1. [kitty](https://sw.kovidgoyal.net/kitty/) as a terminal
+2. [zsh](https://www.zsh.org/) as a shell
+3. [Oh my zsh](https://ohmyz.sh/) for plugins
+4. [powerlevel10k](https://github.com/romkatv/powerlevel10k) for theme
--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 sudo apt install -s neofetch zsh kitty
-cp ./kitty/kitty.conf $HOME/.config/kitty/
+
+# install OhMyZsh framework (ohmyz.sh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+mkdir -p $HOME/.config/kitty/
+cp kitty.conf $HOME/.config/kitty/
