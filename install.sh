5ad4db25d0d9d5e084b57a1cbba5a3abc46ca2f5
Edoardo Soloperto <edoardo@poweringsrl.it>
2022-04-15 10:19:05 +0200

    add zsh-autosuggestions plugin

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # install powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/plugins}/zsh-autosuggestions
 
 mkdir -p $HOME/.config/kitty/
 cp kitty.conf $HOME/.config/kitty/
